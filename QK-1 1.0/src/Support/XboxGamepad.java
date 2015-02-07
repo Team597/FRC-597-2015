0f13acedf692043f69cc68dbb6a373ea957e3835
emanumarqu <emmarqu@gmail.com>
2015-02-06 18:40:42 -0800

    Added xbox gamepad object.

---
+++ QK-1 1.0/src/Support/XboxGamepad.java
@@ -0,0 +1,13 @@
+package Support;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class XboxGamepad {
+	Joystick xboxGamepad;
+	
+	public XboxGamepad(Joystick xbox){
+		xboxGamepad = xbox;
+	}
+	
+	
+}
