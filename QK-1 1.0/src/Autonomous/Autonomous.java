b2f68029bafa5fab225442abbcd23c6aa34e1330
chromefox4 <rhector300@gmail.com>
2015-02-07 14:25:06 -0800

    Finished timer delays to state machines in autonomous

--- QK-1 1.0/src/Autonomous/Autonomous.java
+++ QK-1 1.0/src/Autonomous/Autonomous.java
@@ -18,7 +18,7 @@ public class Autonomous {
 	DoubleSolenoid omniDirection;
 	DoubleSolenoid solenoidClawLeft;
 	DoubleSolenoid solenoidClawRight;
-	long autonomousPrint = System.currentTimeMillis();
+	long autoPrint = System.currentTimeMillis();
 
 	Drive robotDriveOmni;
 	Claw robotClaw;
@@ -50,6 +50,15 @@ public class Autonomous {
 		autoElevator.autoDrop();
 		solenoidClawLeft.set(Value.kForward);
 		solenoidClawRight.set(Value.kForward);
+		autoTimer.start();
+	}
+
+	public void autoPrint() {
+		if (System.currentTimeMillis() >= autoPrint) {
+			System.out.println("autonomous State: " + autonomous);
+
+			autoPrint += 1000;
+		}
 
 	}
 
@@ -215,121 +224,208 @@ public class Autonomous {
 
 				autoState = 7;
 			}
-			Timer.delay(.3); // closes claw on tote
-
-			autoElevator.autoLift(); // lifts tote
-
-			talonLeft.set(1);
-			talonRight.set(1);
-			Timer.delay(4);// moves forward to get mobility bonus
-			if (autonomous == 5) {
-
-				solenoidClawLeft.set(Value.kReverse);
-				solenoidClawRight.set(Value.kReverse);
-				Timer.delay(.3); // closes claw on the tote
-
-				talonLeft.set(-1);
-				talonRight.set(-1);
-				Timer.delay(1); // moves back
-
+			if (autoState == 7 && autoTimer.get() >= 5.9) {
 				autoElevator.autoLift(); // lifts tote
 
-				omniTalon.set(-1);
-				Timer.delay(2); // moves left
-
-				solenoidClawLeft.set(Value.kForward);
-				solenoidClawRight.set(Value.kForward);
-				Timer.delay(.3); // opens claw
-
+				autoState = 8;
+			}
+			if (autoState == 8 && autoTimer.get() >= 6.9) {
 				talonLeft.set(1);
 				talonRight.set(1);
-				Timer.delay(1); // moves forward into tote
 
-				solenoidClawLeft.set(Value.kReverse);
-				solenoidClawRight.set(Value.kReverse);
-				Timer.delay(.3); //
+				autoState = 9;
+			}
+			if (autoState == 9 && autoTimer.get() >= 10.9) {
+				talonLeft.set(0);
+				talonRight.set(0);
 
-				autoElevator.autoLift();
+			}
+			if (autonomous == 5) {
 
-				talonLeft.set(1);
-				talonRight.set(1);
-				Timer.delay(1.5);
+				if (autoState == 0) {
+					solenoidClawLeft.set(Value.kReverse);
+					solenoidClawRight.set(Value.kReverse);
 
-				omniTalon.set(-1);
-				Timer.delay(1);
+					autoState = 1;
+				}
 
-				talonLeft.set(1);
-				talonRight.set(1);
-				Timer.delay(2.5);
+				if (autoState == 1 && autoTimer.get() >= 0.3) {
+					talonLeft.set(-1);
+					talonRight.set(-1);
+
+					autoState = 2;
+				}
+
+				if (autoState == 2 && autoTimer.get() >= 1.3) {
+					autoElevator.autoLift(); // lifts tote
+
+					autoState = 3;
+				}
+
+				if (autoState == 3 && autoTimer.get() >= 2.3) {
+					omniTalon.set(-1);
+
+					autoState = 4;
+				}
+
+				if (autoState == 4 && autoTimer.get() >= 4.3) {
+					solenoidClawLeft.set(Value.kForward);
+					solenoidClawRight.set(Value.kForward);
+
+					autoState = 5;
+				}
+
+				if (autoState == 5 && autoTimer.get() >= 4.6) {
+					talonLeft.set(1);
+					talonRight.set(1);
+
+					autoState = 6;
+				}
+
+				if (autoState == 6 && autoTimer.get() >= 5.6) {
+
+					solenoidClawLeft.set(Value.kReverse);
+					solenoidClawRight.set(Value.kReverse);
+
+					autoState = 7;
+				}
+				if (autoState == 7 && autoTimer.get() >= 5.9) {
+					autoElevator.autoLift();
+
+					autoState = 8;
+				}
+				if (autoState == 8 && autoTimer.get() >= 6.9) {
+					talonLeft.set(1);
+					talonRight.set(1);
+
+					autoState = 9;
+				}
+				if (autoState == 9 && autoTimer.get() >= 7.4) {
+					omniTalon.set(-1);
+
+					autoState = 10;
+				}
+				if (autoState == 10 && autoTimer.get() >= 8.4) {
+					talonLeft.set(1);
+					talonRight.set(1);
+
+					autoState = 11;
+				}
+				if (autoState == 11 && autoTimer.get() >= 9.4) {
+					talonLeft.set(0);
+					talonRight.set(0);
+				}
 			}
 			if (autonomous == 6) {
 
-				solenoidClawLeft.set(Value.kReverse);
-				solenoidClawRight.set(Value.kReverse);
-				Timer.delay(.3);
+				if (autoState == 0) {
+					solenoidClawLeft.set(Value.kReverse);
+					solenoidClawRight.set(Value.kReverse);
 
-				autoElevator.autoLift();
+					autoState = 1;
+				}
 
-				talonLeft.set(1);
-				talonRight.set(1);
-				Timer.delay(1);
+				if (autoState == 1 && autoTimer.get() >= 0.3) {
+					autoElevator.autoLift();
 
-				omniTalon.set(-1);
-				Timer.delay(2);
+					autoState = 2;
+				}
 
-				solenoidClawLeft.set(Value.kForward);
-				solenoidClawRight.set(Value.kForward);
-				Timer.delay(.3);
+				if (autoState == 2 && autoTimer.get() >= 1.3) {
+					talonLeft.set(1);
+					talonRight.set(1);
 
-				talonLeft.set(1);
-				talonRight.set(1);
-				Timer.delay(1);
+					autoState = 3;
+				}
 
-				solenoidClawLeft.set(Value.kReverse);
-				solenoidClawRight.set(Value.kReverse);
-				Timer.delay(.3);
+				if (autoState == 3 && autoTimer.get() >= 2.3) {
+					omniTalon.set(-1);
 
-				autoElevator.autoDrop();
-				autoElevator.autoLift();
+					autoState = 4;
+				}
 
-				talonLeft.set(-1);
-				talonRight.set(-1);
-				Timer.delay(1);
+				if (autoState == 4 && autoTimer.get() >= 4.3) {
+					solenoidClawLeft.set(Value.kForward);
+					solenoidClawRight.set(Value.kForward);
 
-				omniTalon.set(-1);
-				Timer.delay(2);
+					autoState = 5;
+				}
 
-				solenoidClawLeft.set(Value.kForward);
-				solenoidClawRight.set(Value.kForward);
-				Timer.delay(.3);
+				if (autoState == 5 && autoTimer.get() >= 4.6) {
 
-				talonLeft.set(1);
-				talonRight.set(1);
-				Timer.delay(1);
+					talonLeft.set(1);
+					talonRight.set(1);
 
-				solenoidClawLeft.set(Value.kReverse);
-				solenoidClawRight.set(Value.kReverse);
-				Timer.delay(.3);
+					autoState = 6;
+				}
 
-				autoElevator.autoDrop();
-				autoElevator.autoLift();
+				if (autoState == 6 && autoTimer.get() >= 5.6)
+					solenoidClawLeft.set(Value.kReverse);
+					solenoidClawRight.set(Value.kReverse);
 
-				talonLeft.set(1);
-				talonRight.set(1);
-				Timer.delay(4);
+					autoState = 7;
+				}
+				if (autoState == 7 && autoTimer.get() > 5.9) {
+					autoElevator.autoDrop();
+					autoElevator.autoLift();
 
+					autoState = 8;
+				}
+
+				if (autoState == 8 && autoTimer.get() >= 6.9) {
+					talonLeft.set(-1);
+					talonRight.set(-1);
+
+					autoState = 9;
+				}
+				if (autoState == 9 && autoTimer.get() >= 7.9) {
+					omniTalon.set(-1);
+
+					autoState = 10;
+				}
+
+				if (autoState == 10 && autoTimer.get() >= 9.9) {
+					solenoidClawLeft.set(Value.kForward);
+					solenoidClawRight.set(Value.kForward);
+
+					autoState = 11;
+				}
+				if (autoState == 11 && autoTimer.get() >= 10.2) {
+					talonLeft.set(1);
+					talonRight.set(1);
+
+					autoState = 12;
+				}
+				if (autoState == 12 && autoTimer.get() >= 11.2) {
+					solenoidClawLeft.set(Value.kReverse);
+					solenoidClawRight.set(Value.kReverse);
+
+					autoState = 13;
+				}
+				if (autoState == 13 && autoTimer.get() >= 11.5) {
+					autoElevator.autoDrop();
+					autoElevator.autoLift();
+
+					autoState = 14;
+				}
+				if (autoState == 14 && autoTimer.get() >= 12.5) {
+					talonLeft.set(1);
+					talonRight.set(1);
+
+					autoState = 15;
+				}
+				if (autoState == 15 && autoTimer.get() >= 14) {
+					talonLeft.set(0);
+					talonRight.set(0);
+				}
 			}
-			if (autonomous == 7) {
+		if (autonomous == 7) {
 
-			}
-			if (autonomous == 8) {
+		}
+		if (autonomous == 8) {
 
-			}
-
-			if (System.currentTimeMillis() >= autonomousPrint) {
-				System.out.println("autonomous State: " + autonomous);
-			}
 		}
 
 	}
+
 }
