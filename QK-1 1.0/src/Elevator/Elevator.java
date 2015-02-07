f930515a14b80759164ba6cbbacbfe4ea3a364fc
emanumarqu <emmarqu@gmail.com>
2015-02-06 19:44:26 -0800

    Made varibales for setpoints in elevator class.

--- QK-1 1.0/src/Elevator/Elevator.java
+++ QK-1 1.0/src/Elevator/Elevator.java
@@ -19,6 +19,15 @@ public class Elevator {
 	DoubleSolenoid brake;
 	
 	int elevatorState = 1;
+	
+	double HEIGHT_AUTONOMOUS = 2.3;
+	double HEIGHT_BOTTOM = 0.0;
+	double HEIGHT_TOTEONE = 3.0;
+	double HEIGH_TOTETWO = 5.0;
+	double HEIGHT_TOTETHREE = 7.0;
+	double HEIGHT_TOTEFOUR = 9.0;
+	double HEIGHT_TOP = 11.0;
+	
 
 	public Elevator() {
 		talonElevator = new Talon(3);
@@ -33,11 +42,11 @@ public class Elevator {
 
 	public void autoDrop() {
 
-		elevator.setSetpoint(0.0); // lowers elevator for autonomous
+		elevator.setSetpoint(HEIGHT_BOTTOM); // lowers elevator for autonomous
 	}
 
 	public void autoLift() {
-		elevator.setSetpoint(2.3); // lifts elevator for autonomous
+		elevator.setSetpoint(HEIGHT_AUTONOMOUS); // lifts elevator for autonomous
 	}
 
 	public void robotInit() {
@@ -66,35 +75,35 @@ public class Elevator {
 	public void teleopPeriodic() {
 
 		if (xboxGamepad.getRawButton(1) == true) {
-			elevator.setSetpoint(0.0); // sets pid to move to base position	
+			elevator.setSetpoint(HEIGHT_BOTTOM); // sets pid to move to base position	
 		}
 		if (xboxGamepad.getRawButton(2) == true) {
-			elevator.setSetpoint(3.0); // sets pid to move to tote 1 position
+			elevator.setSetpoint(HEIGHT_TOTEONE); // sets pid to move to tote 1 position
 		}
 		if (xboxGamepad.getRawButton(3) == true) {
-			elevator.setSetpoint(5.0); // sets pid to move to tote 2 position
+			elevator.setSetpoint(HEIGH_TOTETWO); // sets pid to move to tote 2 position
 		}
 		if (xboxGamepad.getRawButton(4) == true) {
-			elevator.setSetpoint(7.0); // sets pid to move to tote 3 position
+			elevator.setSetpoint(HEIGHT_TOTETHREE); // sets pid to move to tote 3 position
 		}
 		if (xboxGamepad.getRawAxis(2) > 0) {
-			elevator.setSetpoint(9.0); // sets pid to move to tote 4 position
+			elevator.setSetpoint(HEIGHT_TOTEFOUR); // sets pid to move to tote 4 position
 		}
 		if (xboxGamepad.getRawAxis(3) > 0) {
-			elevator.setSetpoint(11.0); // sets pid to move to top position
+			elevator.setSetpoint(HEIGHT_TOP); // sets pid to move to top position
 		}
 		
 	
 		if(topSwitch.get() == true){
-			elevator.setSetpoint(9.0); // if top switch is pressed lowers elevator
+			elevator.setSetpoint(HEIGHT_TOTEFOUR); // if top switch is pressed lowers elevator
 		}
 		if(botSwitch.get() == true){
-			elevator.setSetpoint(3.0); // if bot switch is pressed lifts elevator
+			elevator.setSetpoint(HEIGHT_TOTEONE); // if bot switch is pressed lifts elevator
 		}
 		
-		if(xboxGamepad.getRawButton(7) == true && botSwitch.get() == false && topSwitch.get() == false){
+		if(xboxGamepad.getRawButton(1) == true && botSwitch.get() == false && topSwitch.get() == false){
 			elevator.disable(); // if button 7 is pressed and no switches are being pressed it changes to manual control
-			talonElevator.set(xboxGamepad.getY());
+			talonElevator.set(xboxGamepad.getY()); //change to another joystick for better control
 		}
 		else{
 			elevator.enable(); // when the conditions are not met enables the PID Controller
