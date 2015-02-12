b381fbc64bee47928047acb908ae8fb3de7ad736
chromefox4 <rhector300@gmail.com>
2015-02-11 17:39:00 -0800

    test code

--- QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
+++ QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
@@ -1,17 +1,11 @@
 package org.usfirst.frc.team597.robot;
 
-import Autonomous.Autonomous;
-import Bane.Claw;
-import Bane.Drive;
-import Elevator.Elevator;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,98 +19,51 @@ public class Robot extends IterativeRobot {
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
-	//ELECTRONIC TEST CODE
-	Joystick joystickLeft;
-	Joystick joystickRight;
-	Talon talonLeft;
-	Talon talonRight;
-	Talon omniTalon;
-	DoubleSolenoid omniDirection;
-	DoubleSolenoid solenoidClawLeft;
-	DoubleSolenoid solenoidClawRight;
-	Compressor compressorAir;
-	
-	Elevator elevator;
-	Autonomous autonomous;
-	Drive robotDriveOmni;
-	Claw robotClaw;
-	
-	Command autonomousCommand;
-	SendableChooser autoChooser;
-	
-	long lastPrint = System.currentTimeMillis();
 
-	public Robot() {
-		joystickLeft = new Joystick(0);
-		joystickRight = new Joystick(1);
-		talonLeft = new Talon(0);
-		talonRight = new Talon(1);
-		omniTalon = new Talon(4);
-		omniDirection = new DoubleSolenoid(4, 5);
-		solenoidClawLeft = new DoubleSolenoid(0, 1);
-		solenoidClawRight = new DoubleSolenoid(2, 7);
-		compressorAir = new Compressor();
-		
-		autonomous = new Autonomous(joystickLeft, joystickLeft, talonLeft, talonRight, omniTalon, omniDirection, solenoidClawLeft, solenoidClawRight);
-		elevator = new Elevator();
-		robotDriveOmni = new Drive(joystickLeft, joystickRight, talonLeft, talonRight, omniTalon, omniDirection);
-		robotClaw = new Claw(joystickLeft, solenoidClawLeft);
-	}
+	Joystick j1 = new Joystick(0);
+	Joystick j2 = new Joystick(1);
+	Joystick j3 = new Joystick(2);
+	Talon t1 = new Talon(0);
+	Talon t2 = new Talon(1);
+	Talon t3 = new Talon(4);
+	Compressor comp = new Compressor();
+	DoubleSolenoid claw = new DoubleSolenoid(0, 7);
+	DoubleSolenoid brake = new DoubleSolenoid(1, 6);
 
 	public void robotInit() {
-		robotDriveOmni.robotInit();
-		elevator.robotInit();
-		
-		autoChooser = new SendableChooser();
-		autoChooser.addDefault("Default program", new Integer(0));
-		autoChooser.addDefault("Autonomous number 1", new Integer(1));
-		autoChooser.addDefault("Autonomous number 2", new Integer(2));
-		autoChooser.addDefault("Autonomous number 3", new Integer(3));
-		autoChooser.addDefault("Autonomous number 4", new Integer(4));
-		autoChooser.addDefault("Autonomous number 5", new Integer(5));
-		autoChooser.addDefault("Autonomous number 6", new Integer(6));
-		autoChooser.addDefault("Autonomous number 7", new Integer(7));
-		autoChooser.addDefault("Autonomous number 8", new Integer(8));
-		
-		SmartDashboard.putData("Autonomous mode chooser", autoChooser);
-		
-	}
-	
-	public void autonomousInit(){
-		// Fintan's special don't-crash try block
-		try {
-			
-			Integer automode = (Integer) autoChooser.getSelected();
-			autonomous.setAutonomous(automode.intValue());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
 	}
 
 	/**
 	 * This function is called periodically during autonomous
 	 */
 	public void autonomousPeriodic() {
-		
-	}
-	
-	public void disabledPeriodic(){
-		
+
 	}
 
 	/**
 	 * This function is called periodically during operator control
 	 */
 	public void teleopPeriodic() {
-		robotDriveOmni.teleopPeriodic();
-		elevator.teleopPeriodic();
-		robotClaw.teleopPeriodic();
-	
-		elevator.elevatorPrint();
-		autonomous.autoPrint();
-		
+
+		t1.set(j1.getY() * -1);
+		t2.set(j2.getY());
+		t3.set(j3.getY());
+
+		if (j1.getRawButton(1) == true) {
+			claw.set(Value.kReverse);
+		} else {
+			claw.set(Value.kForward);
+		}
+
+		if (j3.getRawButton(1) == true) {
+			brake.set(Value.kForward);
+		} else {
+			brake.set(Value.kReverse);
+		}
 	}
 
+	
 	/**
 	 * This function is called periodically during test mode
 	 */
