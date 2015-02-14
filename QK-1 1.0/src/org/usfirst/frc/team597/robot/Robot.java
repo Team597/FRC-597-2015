a5704cd3c8cf2ed86c56781341c9e3ca0d3ba0f7
chromefox4 <rhector300@gmail.com>
2015-02-13 21:03:00 -0800

    Added PID limit toggle, setpoints, bottom setpoint, top setpoint.

--- QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
+++ QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
@@ -29,7 +29,7 @@ public class Robot extends IterativeRobot {
 	Joystick j4 = new Joystick(3);
 	Talon t1 = new Talon(0);
 	Talon t2 = new Talon(1);
-	Talon oT = new Talon(2); // not connected on roboRio
+	Talon oT = new Talon(2);
 	Talon t3 = new Talon(3);
 	Compressor comp = new Compressor();
 	DoubleSolenoid claw = new DoubleSolenoid(0, 7);
@@ -39,16 +39,27 @@ public class Robot extends IterativeRobot {
 	Encoder MM = new Encoder(1, 2);
 	Encoder LT = new Encoder(5, 6);
 	Encoder RT = new Encoder(3, 4);
-	PIDController elev = new PIDController(-1/100.0, 0, 0, en1, t3);
+	PIDController elev = new PIDController(-1 / 100.0, 0, 0, en1, t3);
 	DigitalInput lBot = new DigitalInput(0);
 	DigitalInput lTop = new DigitalInput(9);
 	int eS = 1;
 	long print = System.currentTimeMillis();
 
-	int TOTEONE = 2000;
-	int TOTETWO = 5;
-	int TOTETHREE = 7;
-	int TOTEFOUR = 9;
+	boolean lastBotState = false;
+	boolean lastTopState = false;
+
+	// Add this to every setpoint to zero it to the robot
+	// This will subtract a bunch if the encoder was zeroed to the top
+	// And be zero if it was zeroed to the bottom.
+	int ENCODER_OFFSET = 0;
+	int DIFFERENCE_TOP_BOTTOM_ENCODER = 3833;
+
+	int BASE = -67;
+	int TOTEONE = 1000;
+	int TOTETWO = 2000;
+	int TOTETHREE = 2500;
+	int TOTEFOUR = 3000;
+	int TOP = 3800;
 
 	public void robotInit() {
 
@@ -66,7 +77,6 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 
-		
 		if (System.currentTimeMillis() >= print) {
 
 			System.out.println("elevator: " + en1.get());
@@ -76,18 +86,26 @@ public class Robot extends IterativeRobot {
 			print = System.currentTimeMillis() + 1000;
 		}
 
-		if (lBot.get() == false) {
-	
-			
+		if (lBot.get() != lastBotState) {
+			en1.reset();
+			ENCODER_OFFSET = 0;
+			// elev.disable();
+		}
+		lastBotState = lBot.get();
+
+		if (lTop.get() != lastTopState) {
 			en1.reset();
 			elev.disable();
+			ENCODER_OFFSET = -DIFFERENCE_TOP_BOTTOM_ENCODER;
 		}
+		lastTopState = lTop.get();
 
-		if (j3.getRawButton(1)) {
+		if (j3.getRawButton(2)) {
 			elev.disable();
 			t3.set(j3.getY());
-		}else{
-			//t3.set(0);
+		} else {
+			// t3.set(0);
+			// 3833
 		}
 		if (j2.getRawButton(1) == true) {
 			OD.set(Value.kReverse);
@@ -110,7 +128,7 @@ public class Robot extends IterativeRobot {
 			t2.set(j2.getY());
 		}
 
-		if (j3.getRawButton(2) == true) {
+		if (j3.getRawButton(7) == true) {
 			claw.set(Value.kReverse);
 		} else {
 			claw.set(Value.kForward);
@@ -124,34 +142,29 @@ public class Robot extends IterativeRobot {
 
 		if (j4.getRawButton(1)) {
 			elev.enable();
-			elev.setSetpoint(TOTEONE);
+			elev.setSetpoint(TOTEONE + ENCODER_OFFSET);
 		}
-		/*
+
 		if (j4.getRawButton(2)) {
-			elev.setSetpoint(TOTETWO);
-			if (en1.get() == TOTETWO) {
-				claw.set(Value.kReverse);
-			} else {
-				claw.set(Value.kForward);
-			}
+			elev.enable();
+			elev.setSetpoint(TOTETWO + ENCODER_OFFSET);
 		}
 		if (j4.getRawButton(3)) {
-			elev.setSetpoint(TOTETHREE);
-			if (en1.get() == TOTETHREE) {
-				claw.set(Value.kReverse);
-			} else {
-				claw.set(Value.kForward);
-			}
+			elev.enable();
+			elev.setSetpoint(TOTETHREE + ENCODER_OFFSET);
 		}
 		if (j4.getRawButton(4)) {
-			elev.setSetpoint(TOTEFOUR);
-			if (en1.get() == TOTEFOUR) {
-				claw.set(Value.kReverse);
-			} else {
-				claw.set(Value.kForward);
-			}
+			elev.enable();
+			elev.setSetpoint(TOTEFOUR + ENCODER_OFFSET);
 		}
-*/
+		if (j4.getRawAxis(2) > 0) {
+			elev.enable();
+			elev.setSetpoint(BASE + ENCODER_OFFSET);
+		}
+		if (j4.getRawAxis(3) > 0) {
+			elev.enable();
+			elev.setSetpoint(TOP + ENCODER_OFFSET);
+		}
 	}
 
 	/**
