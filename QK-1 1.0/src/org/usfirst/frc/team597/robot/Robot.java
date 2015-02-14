19e924d4782f962bf4c4bac574f52bb5b88ce5d3
chromefox4 <rhector300@gmail.com>
2015-02-13 18:04:21 -0800

    PID Controller

--- QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
+++ QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -27,36 +26,32 @@ public class Robot extends IterativeRobot {
 	Joystick j1 = new Joystick(0);
 	Joystick j2 = new Joystick(1);
 	Joystick j3 = new Joystick(2);
+	Joystick j4 = new Joystick(3);
 	Talon t1 = new Talon(0);
 	Talon t2 = new Talon(1);
 	Talon oT = new Talon(2);
-	Talon t3 = new Talon(4);
+	Talon t3 = new Talon(3);
 	Compressor comp = new Compressor();
 	DoubleSolenoid claw = new DoubleSolenoid(0, 7);
 	DoubleSolenoid brake = new DoubleSolenoid(1, 6);
 	DoubleSolenoid OD = new DoubleSolenoid(2, 5);
-	Encoder en1 = new Encoder(0, 1);
+	Encoder en1 = new Encoder(7, 8);
+	Encoder MM = new Encoder(1, 2);
+	Encoder LT = new Encoder(5, 6);
+	Encoder RT = new Encoder(3, 4);
 	PIDController elev = new PIDController(0, 0, 0, en1, t3);
-	DigitalInput lBot = new DigitalInput(2);
+	DigitalInput lBot = new DigitalInput(0);
+	DigitalInput lTop = new DigitalInput(9);
 	int eS = 1;
+	long print = System.currentTimeMillis();
+
+	double TOTEONE = 3.0;
+	double TOTETWO = 5.0;
+	double TOTETHREE = 7.0;
+	double TOTEFOUR = 9.0;
 
 	public void robotInit() {
 
-		eS = 1;
-		if (eS == 1) {
-			t3.set(-0.5);
-			if (lBot.get() == true) {
-				t3.set(0);
-				en1.reset();
-				eS = 2;
-			}
-			if (eS == 2) {
-				t3.set(.80);
-				Timer.delay(1);
-				elev.enable();
-			}
-
-		}
 	}
 
 	/**
@@ -71,21 +66,47 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 
-		t1.set(j1.getY() * -1);
-		t2.set(j2.getY());
-		if (j2.getRawButton(2)) {
-			t3.set(j3.getY());
+		if (System.currentTimeMillis() >= print) {
 
+			System.out.println("elevator: " + en1.get());
+
+			print += 1000;
 		}
-		if (j2.getRawButton(1)) {
-			OD.set(Value.kForward);
-			oT.set(j2.getX());
+
+		if (lBot.get() == true) {
+			en1.reset();
+			elev.enable();
+			elev.setSetpoint(TOTEONE);
+		}
+
+		if(j3.getRawButton(1)){
+			elev.disable();
+			t3.set(j3.getY());
+			
+		
+		}
+		if (j2.getRawButton(1) == true) {
+			OD.set(Value.kReverse);
+			oT.set(j2.getX() / 2);
 			t1.set(0);
 			t2.set(0);
 
+			if (j1.getRawButton(1)) {
+				OD.set(Value.kReverse);
+				oT.set(j2.getX());
+				t1.set(0);
+				t2.set(0);
+
+			}
+
+		} else {
+			OD.set(Value.kForward);
+			oT.set(0);
+			t1.set(j1.getY() * -1);
+			t2.set(j2.getY());
 		}
 
-		if (j3.getRawButton(10) == true) {
+		if (j3.getRawButton(2) == true) {
 			claw.set(Value.kReverse);
 		} else {
 			claw.set(Value.kForward);
@@ -97,25 +118,39 @@ public class Robot extends IterativeRobot {
 			brake.set(Value.kReverse);
 		}
 
-		if (j3.getRawButton(2)) {
-			elev.setSetpoint(2.0);
+		if (j4.getRawButton(1)) {
+			elev.setSetpoint(TOTEONE);
+			if (en1.get() == TOTEONE) {
+				claw.set(Value.kReverse);
+			} else {
+				claw.set(Value.kForward);
+			}
 		}
-		if (j3.getRawButton(3)) {
-			elev.setSetpoint(4.0);
+		if (j4.getRawButton(2)) {
+			elev.setSetpoint(TOTETWO);
+			if (en1.get() == TOTETWO) {
+				claw.set(Value.kReverse);
+			} else {
+				claw.set(Value.kForward);
+			}
 		}
-		if (j3.getRawButton(4)) {
-			elev.setSetpoint(5.0);
+		if (j4.getRawButton(3)) {
+			elev.setSetpoint(TOTETHREE);
+			if (en1.get() == TOTETHREE) {
+				claw.set(Value.kReverse);
+			} else {
+				claw.set(Value.kForward);
+			}
 		}
-		if (j3.getRawButton(5)) {
-			elev.setSetpoint(6.0);
+		if (j4.getRawButton(4)) {
+			elev.setSetpoint(TOTEFOUR);
+			if (en1.get() == TOTEFOUR) {
+				claw.set(Value.kReverse);
+			} else {
+				claw.set(Value.kForward);
+			}
 		}
-		if (j2.getRawButton(2)) {
-			elev.disable();
-			t3.set(0);
-		}
-		if (j3.getRawButton(7)) {
-			elev.enable();
-		}
+
 	}
 
 	/**
