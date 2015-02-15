9277b2313630913c5f43526e116acfa5a3877c1d
chromefox4 <rhector300@gmail.com>
2015-02-14 16:50:49 -0800

    Mid scrimage code pid moved to j3 because not enough usb ports

--- QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
+++ QK-1 1.0/src/org/usfirst/frc/team597/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -37,6 +38,10 @@ public class Robot extends IterativeRobot {
 	DoubleSolenoid claw = new DoubleSolenoid(1, 6);
 	DoubleSolenoid OD = new DoubleSolenoid(2, 5);
 	Encoder en1 = new Encoder(7, 8);
+	
+	//DigitalInput test7 = new DigitalInput(7);
+	//DigitalInput test8 = new DigitalInput(8);
+	
 	Encoder MM = new Encoder(1, 2);
 	Encoder LT = new Encoder(5, 6);
 	Encoder RT = new Encoder(3, 4);
@@ -55,19 +60,19 @@ public class Robot extends IterativeRobot {
 	int DIFFERENCE_TOP_BOTTOM_ENCODER = 3813;
 
 	int BASE = -67;
-	int TOTEONE = 1000;
-	int TOTETWO = 2000;
-	int TOTETHREE = 2500;
+	int TOTEONE = 9;
+	int TOTETWO = 1200;
+	int TOTETHREE = 1900;
 	int TOTEFOUR = 3000;
 	int TOP = 3800;
-
+ 
 	int autonomous = 0;
 	int maxAutonomous = 10;
 	int autoState = 0;
 	Timer autoTimer;
 
 	public void robotInit() {
-		elev.setAbsoluteTolerance(50);
+	//	elev.setAbsoluteTolerance(50);
 		
 	}
 
@@ -81,8 +86,10 @@ public class Robot extends IterativeRobot {
 	 * This function is called periodically during autonomous
 	 */
 	public void autonomousPeriodic() {
-
-		
+		t1.set(0);
+		t2.set(0);
+		t3.set(0);
+		/*
 		if (autoState == 0) {
 			t1.set(0.5);
 			t2.set(-0.5); // moves forward
@@ -95,8 +102,7 @@ public class Robot extends IterativeRobot {
 		}
 		if (autonomous == 2) {
 
-			claw.set(Value.kReverse);
-			brake.set(Value.kReverse);
+			claw.set(Value.kForward);
 			Timer.delay(.3); // closes claw may be used on totes and containers
 
 			 // lifts elevator
@@ -115,7 +121,6 @@ public class Robot extends IterativeRobot {
 			// v
 			
 			claw.set(Value.kReverse);
-			brake.set(Value.kReverse);
 			Timer.delay(.3); // closes claw on the tote
 
 			t1.set(-1);
@@ -129,7 +134,6 @@ public class Robot extends IterativeRobot {
 			Timer.delay(2); // moves left
 
 			claw.set(Value.kForward);
-			brake.set(Value.kForward);
 			Timer.delay(.3); // opens claw
 
 			t1.set(-1);
@@ -140,7 +144,6 @@ public class Robot extends IterativeRobot {
 			elev.setSetpoint(TOTETWO + ENCODER_OFFSET);
 
 			claw.set(Value.kReverse);
-			brake.set(Value.kReverse);
 			Timer.delay(.3); // closes claw on tote
 
 			elev.enable();
@@ -150,7 +153,7 @@ public class Robot extends IterativeRobot {
 			t2.set(1);
 			Timer.delay(4); // moves forward to get mobility bonus
 		}
-
+*/
 	}
 
 	/**
@@ -158,15 +161,24 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 
+		SmartDashboard.putNumber("Elevator Encoder", en1.get());
+		
 		if (System.currentTimeMillis() >= print) {
 
 			System.out.println("elevator: " + en1.get());
+			
+			/*
+			System.out.println("test 7: "+test7.get());
+			System.out.println("test 8: "+test8.get());
+			*/
 			System.out.println("Bottm limit switch hit: " + lBot.get());
 			System.out.println("Top limit switch hit: " + lTop.get());
 
 			print = System.currentTimeMillis() + 500;
 		}
 
+		
+		
 		if (lBot.get() != lastBotState) {
 			int error = en1.get() + DIFFERENCE_TOP_BOTTOM_ENCODER;
 
@@ -183,6 +195,14 @@ public class Robot extends IterativeRobot {
 		 * System.out.println("Topswitch has been RESET :)"); } lastTopState =
 		 * lTop.get();
 		 */
+		
+		if(j4.getRawButton(6)){
+			claw.set(Value.kReverse);
+		}
+		if(j4.getRawButton(8)){
+			claw.set(Value.kForward);
+		}
+		
 		if (j3.getRawButton(2)) {
 			elev.disable();
 			t3.set(j3.getY());
@@ -211,43 +231,47 @@ public class Robot extends IterativeRobot {
 
 		if (j3.getRawButton(1) == true) {
 			claw.set(Value.kForward);
-		} else {
+		}
+		if (j3.getRawButton(4)) {
 			claw.set(Value.kReverse);
 		}
-
-		if (j4.getRawButton(3)) {
+		
+		if (j3.getRawButton(6)) {
 			elev.enable();
 			elev.setSetpoint(TOTEONE + ENCODER_OFFSET);
 			brake.set(Value.kForward);
 		}
 
-		if (j4.getRawButton(3)) {
+		if (j3.getRawButton(7)) {
 			elev.enable();
 			elev.setSetpoint(TOTETWO + ENCODER_OFFSET);
 			brake.set(Value.kForward);
 		}
-		if (j4.getRawButton(1)) {
+		if (j3.getRawButton(10)) {
 			elev.enable();
 			elev.setSetpoint(TOTETHREE + ENCODER_OFFSET);
 			brake.set(Value.kForward);
 		}
-		if (j4.getRawAxis(2) > 0) {
+		if (j4.getRawButton(2)){
+		//if (j4.getRawAxis(2) > 0) {
 			elev.enable();
 			elev.setSetpoint(TOTEFOUR + ENCODER_OFFSET);
 			brake.set(Value.kForward);
 		}
-		if (j4.getRawButton(2)) {
+		if (j3.getRawButton(11)) {
+		//if (j4.getRawButton(2)) {
 			elev.enable();
 			elev.setSetpoint(BASE + ENCODER_OFFSET);
 			brake.set(Value.kForward);
 		}
-		if (j4.getRawAxis(3) > 0) {
+		if (j3.getRawAxis(3) >  0 || j3.getRawButton(3)) {
 			
 			brake.set(Value.kReverse);
 			elev.disable();
 			t3.set(0);
 			
 		}
+		
 	}
 
 	/**
